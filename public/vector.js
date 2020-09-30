4255c5accbaba306e3126f2981688d22ca08d342
da284 <danielkk7@gmail.com>
2020-09-30 13:42:52 +0300

    changed vector

--- public/vector.js
+++ public/vector.js
@@ -12,5 +12,69 @@ rotate(angle): rotate vector by angle
 */
 
 class Vector {
-    
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+	negative() {
+		this.x = -this.x
+		this.y = -this.y
+		return this;
+	}
+
+	add(v) {
+		return new Vector(this.x + v.x, this.y + other.y)
+	}
+
+	subtract(v) {
+		return new Vector(this.x - v.x, this.y - other.y)
+	}
+
+	multiply(v) {
+		return new Vector(this.x * v.x + this.y * v.y)
+	}
+
+	equals(v) {
+		return this.x == v.x && this.y == v.y;
+	}
+
+	dot(v) {
+		return this.x * v.x + this.y * v.y;
+	}
+
+	mag() {
+		return Math.sqrt(this.dot(this));
+	}
+
+	set_angle(a){
+		magnitude = this.mag()
+		this.x = Math.cos(a) * magnitude
+		this.y = Math.sin(a) * magnitude
+	}
+
+	rotate(a){
+		b = this.toAngles()
+		this.set_angle((a + b) % 360)
+	}
+
+	min() {
+		return Math.min(this.x, this.y);
+	}
+
+	max() {
+		return Math.max(this.x, this.y);
+	}
+
+	toAngles() {
+		return Math.atan2(this.y, this.x);
+	}
+
+	toArray() {
+		return [this.x, this.y]
+	}
+
+	clone() {
+		return new Vector(this.x, this.y);
+	}
 }
